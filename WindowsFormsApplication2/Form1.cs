3956015fd82cb6055147232c17f974aa9fa5476e
Oleg <S0HardCore@users.noreply.github.com>
2016-03-18 22:09:54 +0200

    180316
    wfa2_18_1a

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+        #region Constants
+        static readonly int
+            MAX_OFFSET_X = 1000,
+            MAX_OFFSET_Y = 1000,
+            HEXAGON_WIDTH = 96,
+            HEXAGON_HEIGHT = 84,
+            HEXAGON_MARGIN = 4;
+        #endregion
+
         class ConsolePrototype
         {
             public Boolean Enabled;
@@ -57,7 +68,7 @@ namespace WindowsFormsApplication2
                             else
                                 if (currentResolution.Height < 600)
                                     currentResolution.Height = 600;
-                            consoleLog = currentResolution.Width + ";" + currentResolution.Height + " done.";
+                            consoleLog = currentResolution.Width + "x" + currentResolution.Height + " done.";
                         }
                         else consoleLog = "Error.";
                     else consoleLog = "Error.";
@@ -69,11 +80,15 @@ namespace WindowsFormsApplication2
                         mouseOffset = new Point(0, 0);
                         consoleLog = "Reset done.";
                         break;
+                    case "RESOLUTION":
+                    case "DEFAULTRESOLUTION":
+                    case "DEFRESOLUTION":
+                    case "DEFAULTRES":
                     case "RES":
                     case "DEFRES":
                         currentResolution.Width = 1920;
                         currentResolution.Height = 1080;
-                        consoleLog = "1920;1080 done.";
+                        consoleLog = "1920x1080 done.";
                         break;
                     //case ""
                 }
@@ -83,16 +98,53 @@ namespace WindowsFormsApplication2
             }
         }
 
+        #region Hexagon
+        static readonly Point[]
+            HexagonPosition = new Point[7]
+            {
+                new Point(100 + HEXAGON_WIDTH / 4, 200 + (HEXAGON_HEIGHT + HEXAGON_MARGIN )/ 2),
+                new Point(200, 200),
+                new Point(200 + 3 * HEXAGON_WIDTH / 4 + HEXAGON_MARGIN, 200 + (HEXAGON_HEIGHT + HEXAGON_MARGIN) / 2),
+                new Point(100 + HEXAGON_WIDTH / 4, 200 + 3 * (HEXAGON_HEIGHT + HEXAGON_MARGIN) / 2),
+                new Point(200, 200 + (HEXAGON_HEIGHT + HEXAGON_MARGIN) * 2),
+                new Point(200 + 3 * HEXAGON_WIDTH / 4 + HEXAGON_MARGIN, 200 + 3 * (HEXAGON_HEIGHT + HEXAGON_MARGIN) / 2),
+                new Point(200, 200 + HEXAGON_HEIGHT + 4)
+            };
+        Boolean[]
+            HexagonHover = new Boolean[7],
+            HexagonKeys = new Boolean[7];
+        List<GraphicsPath> Hexagon = new List<GraphicsPath>()
+            {
+                new GraphicsPath(),
+                new GraphicsPath(),
+                new GraphicsPath(),
+                new GraphicsPath(),
+                new GraphicsPath(),
+                new GraphicsPath(),
+                new GraphicsPath()
+            };
+        Color[] HexagonColors = new Color[7]
+            {
+                Color.FromArgb(101, 176, 49),
+                Color.FromArgb(255, 254, 52),
+                Color.FromArgb(251, 153, 2),
+                Color.FromArgb(3, 146, 206),
+                Color.FromArgb(135, 1, 176),
+                Color.FromArgb(254, 39, 19),
+                Color.Gray
+            };
+        string[] HexagonText = new string[7] { "Q", "W", "E", "A", "S", "D", "Space" };
+        string HexagonString = "";
+        #endregion
+
         ConsolePrototype 
             Console = new ConsolePrototype();
         Font
+            Verdana16 = new Font("Verdana", 16),
             Verdana13 = new Font("Verdana", 13),
             Verdana11 = new Font("Verdana", 11);
         Timer
             timer = new Timer();
-        static readonly int
-            MAX_OFFSET_X = 1000,
-            MAX_OFFSET_Y = 1000;
         static Point
             mouseOffset = new Point(0, 0),
             mousePosition = new Point(960, 540);
@@ -100,6 +152,7 @@ namespace WindowsFormsApplication2
             currentResolution = new Size(1920, 1080);
         Boolean
             mDown;
+        
 
         public Form1()
         {
@@ -115,22 +168,61 @@ namespace WindowsFormsApplication2
             timer.Interval = 1;
             timer.Tick += new EventHandler(pUpdate);
             timer.Start();
-            Cursor.Hide();
+            //Cursor.Hide();
+            for (int a = 0; a < 7; ++a)
+            {
+                Hexagon[a].AddPolygon(BuildHexagon(HexagonPosition[a]));
+            }
         }
 
         void pMouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
+            {
                 mDown = true;
+                Point temp = ConvertClickPoint(e.X, e.Y);
+                for (int q = 0; q < 7; ++q)
+                {
+                    HexagonHover[q] = Hexagon[q].IsVisible(temp);
+                    if (HexagonHover[q])
+                        if (q != 6)
+                            HexagonString += HexagonText[q];
+                        else
+                            HexagonString = "";
+                    }
+            }
+
         }
 
         void pMouseUp(object sender, MouseEventArgs e)
         {
             mDown = false;
+            Point temp = ConvertClickPoint(e.X, e.Y);
+            for (int q = 0; q < 7; ++q)
+            {
+                HexagonHover[q] = false;
+                //if (HexagonHover[q])
+                //    if (q != 6)
+                //        HexagonString += HexagonText[q];
+                //    else
+                //        HexagonString = "";
+            }
         }
 
         void pMouseMove(object sender, MouseEventArgs e)
         {
+            Point temp = ConvertClickPoint(e.X, e.Y);
+            if (e.Button == MouseButtons.Left)
+            //for (int q = 0; q < 7; ++q)
+            //{
+            //    HexagonHover[q] = Hexagon[q].IsVisible(temp);
+            //    if (HexagonHover[q])
+            //        if (q != 6)
+            //            HexagonString += HexagonText[q];
+            //        else
+            //            HexagonString = "";
+            //}
+
             if (e.Button == MouseButtons.Left)
             {
                 if (mouseOffset.X > -MAX_OFFSET_X)
@@ -150,8 +242,24 @@ namespace WindowsFormsApplication2
                 else
                     mouseOffset.Y = MAX_OFFSET_Y;
             }
+            /*if (e.X != mousePosition.X && e.Y != mousePosition.Y)
+            {
+                int qw = mousePosition.X - e.X,
+                    we = mousePosition.Y - e.Y;
+                qw = e.X - qw;
+                we = e.Y - we;
+                Cursor.Position = new Point(qw, we);
+            }*/
             mousePosition.X = e.X;
             mousePosition.Y = e.Y;
+        }
+
+        Point ConvertClickPoint(int eX, int eY)
+        {
+            int a = (int)((eX - mouseOffset.X) / GetResolutionRatio().Width),
+                b = (int)((eY - mouseOffset.Y) / GetResolutionRatio().Height);
+            Point temp = new Point(a, b);
+            return temp;
         }
 
         void pKeyDown(object sender, KeyEventArgs e)
@@ -182,6 +290,14 @@ namespace WindowsFormsApplication2
                         break;
                 }
             }
+            else
+            {
+                for (int a = 0; a < 6; ++a)
+                    if ((char)e.KeyValue == HexagonText[a][0])
+                        HexagonHover[a] = true;
+                if (e.KeyData == Keys.Space)
+                    HexagonHover[6] = true;
+            }
         }
 
 
@@ -199,7 +315,7 @@ namespace WindowsFormsApplication2
                     else
                     {
                         Console.Enabled = false;
-                        if (Console.getLength() > 0)
+                        if (!String.IsNullOrEmpty(Console.getString()))
                         {
                             string temp = Console.getString();
                             temp = temp.Remove(0);
@@ -208,13 +324,45 @@ namespace WindowsFormsApplication2
                     }
                     break;
             }
+            for (int a = 0; a < 6; ++a)
+                if ((char)e.KeyValue == HexagonText[a][0])
+                {
+                    HexagonHover[a] = false;
+                    HexagonString += (char)e.KeyValue;
+                }
+            if (e.KeyData == Keys.Space)
+            {
+                HexagonHover[6] = false;
+                HexagonString = "";
+            }
         }
 
         void pUpdate(object sender, EventArgs e)
         {
-            if (!mDown)
-                Cursor.Position = new Point(960, 540);
+            //if (!mDown)
+                //Cursor.Position = new Point(960, 540);
+            //mouseOffset.X +=2;
             Invalidate();
+        }
+
+        SizeF GetResolutionRatio()
+        {
+            float a = (float)(currentResolution.Width / 1920f);
+            float b = (float)(currentResolution.Height / 1080f);
+            return new SizeF(a, b);
+        }
+
+        Point[] BuildHexagon(Point start)
+        {
+            int width = HEXAGON_WIDTH, height = HEXAGON_HEIGHT;
+            Point[] HexPoints = new Point[6];
+            HexPoints[0] = new Point(start.X + width / 4, start.Y);
+            HexPoints[1] = new Point(start.X + 3 * width / 4, start.Y);
+            HexPoints[2] = new Point(start.X + width, start.Y + height / 2);
+            HexPoints[3] = new Point(start.X + 3 * width / 4, start.Y + height);
+            HexPoints[4] = new Point(start.X + width / 4, start.Y + height);
+            HexPoints[5] = new Point(start.X, start.Y + height / 2);
+            return HexPoints;
         }
 
         Boolean TriangleContainsPoint(Point[] TP, Point PP)
@@ -233,12 +381,10 @@ namespace WindowsFormsApplication2
         void pDraw(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-
+            
             #region Resolution
             g.TranslateTransform(mouseOffset.X, mouseOffset.Y);
-            float a = (float)(currentResolution.Width / 1920f);
-            float b = (float)(currentResolution.Height / 1080f);
-            g.ScaleTransform(a, b);
+            g.ScaleTransform(GetResolutionRatio().Width, GetResolutionRatio().Height);
             //if (this.Size.Width < currentResolution.Width || this.Size.Height < currentResolution.Height)
                 //this.Size = new Size(currentResolution.Width, currentResolution.Height);
             #endregion
@@ -246,9 +392,23 @@ namespace WindowsFormsApplication2
             Point[] TP = new Point[] { new Point (960, -20), new Point (-20, 1100), new Point (1940, 1100) };
             g.FillPolygon(Brushes.CornflowerBlue, TP);
             g.DrawRectangle(Pens.DarkSlateBlue, -200, -400, 300, 700);
+            
+
+            g.DrawPolygon(Pens.Black, TP);
+
+            for (int a = 0; a < 7; ++a)
+            {
+                if (HexagonHover[a])
+                    g.FillPath(new SolidBrush(HexagonColors[a]), Hexagon[a]);
+                else
+                    g.DrawPath(Pens.Black, Hexagon[a]);
+                Point TPS = HexagonPosition[a];
+                TPS.Offset(36 - (a == 6 ? 22 : a == 1 ? 2 : 0), 30);
+                g.DrawString(HexagonText[a], Verdana16, Brushes.Black, TPS);
+            }
             g.ResetTransform();
 
-            g.DrawString("CurPos:\n" + mousePosition.ToString() + "\nOffset: \n" + mouseOffset.ToString() + "\nString:" + Console.getString() + "\nMonitorRes:\n" + Screen.PrimaryScreen.Bounds.Size.ToString() + "\nCurRes:\n" + currentResolution.ToString(), Verdana11, Brushes.Black, 0, 0);
+            g.DrawString("Cursor Position:\n" + mousePosition.ToString() + "\nOffset: \n" + mouseOffset.ToString() + "\nConsole String:" + Console.getString() + "\nHexagon String:" + HexagonString + "\nMonitor Resolution:\n" + Screen.PrimaryScreen.Bounds.Size.ToString() + "\nApp Resolution:\n" + currentResolution.ToString(), Verdana11, Brushes.Black, 0, 0);
 
             if (Console.Enabled)
             #region Console
